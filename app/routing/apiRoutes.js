d808a91ce950d7e89e2de26d78725fb97fc9388b
Alexis San Javier <alexissj16@knights.ucf.edu>
2020-06-30 08:46:55 -0400

    completed matching algorithm

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,3 @@
-const path = require("path");
 const friends = require("../data/friends");
 
 module.exports = (app) => {
@@ -7,6 +6,24 @@ module.exports = (app) => {
   });
 
   app.post("/api/friends", (req, res) => {
-    res.send("friends post");
+    res.json(findMatch(req.body));
   });
+
+  const findMatch = (user) => {
+    let bestMatch = [Number.MAX_VALUE, null];
+
+    friends.forEach((friend, i) => {
+      let match = compare(user.scores, friend.scores);
+
+      if (match < bestMatch[0]) {
+        bestMatch = [match, i];
+      }
+    });
+
+    return friends[bestMatch[1]];
+  };
+
+  const compare = (a, b) => {
+    return a.reduce((total, score, i) => total + Math.abs(score - b[i]), 0);
+  };
 };
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 
-const port = 8080;
+const port = 8080 || process.env.PORT;
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 require("./app/routing/htmlRoutes")(app);
 require("./app/routing/apiRoutes")(app);
