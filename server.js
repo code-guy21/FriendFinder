bf7d87461bbe5da366d298a8619738b294bba180
Alexis San Javier <alexissj16@knights.ucf.edu>
2020-07-01 13:29:04 -0400

    applied css styling to home page

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const path = require("path");
 
 const port = 8080 || process.env.PORT;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname + "/app/public")));
 
 require("./app/routing/htmlRoutes")(app);
 require("./app/routing/apiRoutes")(app);
